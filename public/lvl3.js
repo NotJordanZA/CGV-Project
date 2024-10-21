9eba0a2dee6f1fa3a3a6f570ff1f239ad4c1e191
ruben <2437157@students.wits.ac.za>
2024-10-21 12:32:02 +0200

    i wish this just worked
    - but it does not

---
+++ public/lvl3.js
@@ -0,0 +1,493 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { level1Config } from './level1.js';
+import { level2Config } from './level2.js';
+import { level3Config } from './level3.js';
+
+
+// CANVAS SETUP
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+var aspect = WIDTH / HEIGHT;
+
+
+// RENDERER SETUP
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor(0xDDDDDD, 1);
+document.body.appendChild(renderer.domElement);
+var scene = new THREE.Scene();
+const gltfLoader = new GLTFLoader();
+
+
+// CAMERA SETUP
+var d = 40; // Frustum size (affects the zoom level)
+var camera = new THREE.OrthographicCamera(-d * aspect, d * aspect, d, -d, 1, 1000);
+// CREATE ISOMETRIC VIEW
+camera.position.set(40, 40, 40); // Adjust these for the desired view
+camera.lookAt(0, 0, 0); // Aim the camera at the origin (where the cube is)
+
+
+// LEVEL CONFIG
+const levelConfig = {
+    levelNumber: 3,
+    groundTexture: './assets/cobblestone/diffuse.png',
+    groundNormal: './assets/cobblestone/normal.png',
+    groundHeight: './assets/cobblestone/height.png',
+    groundSpecular: './assets/cobblestone/specular.png',
+    cubeColor: 0xADD8E6,
+    flashLightColor: 0xADD8E6,
+    flashLightBounceColor: 0xADD8E6,
+    flashLightPower: 10000,
+};
+
+let currentLevel = 0;
+
+
+// INITIAL POSITIONS
+let initialCubePosition = new THREE.Vector3(0, 0, 0);
+let initialCameraPosition = new THREE.Vector3(40, 40, 40);
+
+
+// DEATH TIMEOUTS
+var darknessTimeout = 100;
+var resetLevelTimeout = 10;
+
+
+// DEATH VISUALS SETUP
+const vignette = document.getElementById('vignette');
+const gameOverMessage = document.getElementById('game-over-message');
+// UPDATE VIGNETTE INTENSITY BASED ON DEATH TIMEOUT
+function updateVignetteIntensity(intensity) {
+    vignette.style.opacity = intensity; // Set opacity between 0 and 1
+}
+// SHOW DEATH SCREEN
+function showGameOverScreen() {
+    gameOverMessage.style.opacity = 1; // Fade in the "You Died" message
+}
+
+
+// RESET THE LEVEL
+export function resetLevel() {
+    // Reset cube position
+    cube.position.copy(initialCubePosition);
+    
+    // Reset camera position
+    camera.position.copy(initialCameraPosition);
+    camera.lookAt(0, 0, 0);  // Make sure camera is looking at the correct point
+    flashTimeout = 5000;
+    bounceTimeout = 100;
+    darknessTimeout = 100;
+    gameOverMessage.style.opacity = 0;
+
+    // Reset vignette opacity to 0 (no vignette)
+    vignette.style.opacity = 0;
+
+    // Optional: Reset any other elements such as lights, textures, etc.
+    setupLevel(); // Reapply the level configurations
+    console.log("Level reset to its original configuration.");
+}
+
+
+// CUBE/CHARACTER SETUP
+var boxGeometry = new THREE.BoxGeometry(10, 10, 10);
+var phongMaterial = new THREE.MeshPhongMaterial({ color: 0x0095DD });
+var cube = new THREE.Mesh(boxGeometry, phongMaterial);
+
+
+// FLASHLIGHT GEOMETRY
+var flashGeometry = new THREE.BoxGeometry(1, 2, 1);
+var flashHolder = new THREE.Mesh(flashGeometry, phongMaterial);
+
+// FLASHLIGHT TIMEOUTS
+var flashTimeout = 5000;
+var bounceTimeout = 100;
+var flickerTimeout = 0;
+
+// FLASHLIGHT SETUP
+var flashLight = new THREE.SpotLight(0xffe394, 5000, 0, Math.PI / 4, 1, 2);
+
+// FLASHLIGHT BOUNCE SETUP
+var flashLightBounce = new THREE.PointLight(0xffe394, 100);
+
+// FLASHLIGHT TARGET SETUP
+var flashLightTarget = new THREE.Object3D();
+
+
+// DECLARE MESH VARIABLES
+let infernoMap;
+let infernoChests;
+let infernoWalls;
+
+// SETUP LEVEL
+function setupLevel() {
+
+    // CUBE PROPERTIES
+    cube.material.color.setHex(levelConfig.cubeColor);
+    cube.rotation.set(0.0, 0.0, 0);
+    cube.translateX(0);
+    scene.add(cube);
+
+    // FLASHLIGHT PROPERTIES
+    flashLight.color.setHex(levelConfig.flashLightColor);
+    flashLight.intensity = levelConfig.flashLightPower;
+    flashLight.position.set(0, 0, 0); // Position it at the cube's location
+
+    // FLASHLIGHT BOUNCE PROPERTIES
+    flashLightBounce.color.setHex(levelConfig.flashLightBounceColor);
+    flashLightBounce.position.set(0, 0, 0);
+
+    // ADD FLASHLIGHT TO SCENE
+    flashLight.add(flashLightBounce);
+    flashHolder.add(flashLight);
+
+    // ADD FLASHLIGHT TARGET TO SCENE
+    scene.add(flashLightTarget);
+    flashLight.target = flashLightTarget;
+
+    // ADD FLASHLIGHT TO PLAYER CHARACTER
+    cube.add(flashHolder); // Attach it to the cube
+
+    // SET BACKGROUND COLOUR
+    scene.background = new THREE.Color( 0x000000 );
+
+    // LOAD THE MESH FOR THE MAP
+    gltfLoader.load('./assets/inferno/cgv-inferno-map-baked-mesh.glb', (gltf) => {
+        // Add the loaded infernoMap to the scene
+        infernoMap = gltf.scene;
+        // Position the infernoMap to the right of the plane
+        infernoMap.rotation.y = -Math.PI / 2;
+        infernoMap.scale.set(50,50,50);
+        infernoMap.position.set(0, -10, 0); // Adjust the position as needed
+        scene.add(infernoMap);
+        floorBoundingBox.setFromObject(infernoMap);
+    }, undefined, (error) => {
+        console.error('An error happened while loading the infernoMap:', error);
+    });
+
+    // LOAD THE MESH FOR THE CHESTS
+    gltfLoader.load('./assets/inferno/cgv-inferno-map-chests-mesh.glb', (gltf) => {
+        // Add the loaded infernoChests to the scene
+        infernoChests = gltf.scene;
+        // Position the infernoChests to the right of the plane
+        infernoChests.rotation.y = -Math.PI / 2;
+        infernoChests.scale.set(50,50,50);
+        infernoChests.position.set(0, -10, 0); // Adjust the position as needed
+        scene.add(infernoChests);
+    }, undefined, (error) => {
+        console.error('An error happened while loading the infernoMap:', error);
+    });
+
+    // LOAD THE MESH FOR THE (INVISIBLE) WALLS
+    gltfLoader.load('./assets/inferno/cgv-inferno-map-walls.glb', (gltf) => {
+        infernoWalls = gltf.scene;
+        infernoWalls.rotation.y = -Math.PI / 2;
+        infernoWalls.scale.set(50,50,50);
+        infernoWalls.position.set(6, -10, 6); // Adjust the position as needed
+    }, undefined, (error) => {
+        console.error('An error happened while loading the infernoMap:', error);
+    });
+
+    // DECLARE LIGHTS AT THE CHESTS
+    var chestLight1 = new THREE.PointLight(0xf76628, 1000);
+    chestLight1.position.set(127,15,-733);
+    var chestLight2 = new THREE.PointLight(0xf76628, 1000);
+    chestLight2.position.set(336,15,230);
+    var chestLight3 = new THREE.PointLight(0xf76628, 1000);
+    chestLight3.position.set(435,15,-172);d
+    var chestLight4 = new THREE.PointLight(0xf76628, 1000);
+    chestLight4.position.set(-672,15,-134);
+    var chestLight5 = new THREE.PointLight(0xf76628, 1000);
+    chestLight5.position.set(-375,15,315);
+
+    // ADD CHEST LIGHTS TO SCENE
+    scene.add(chestLight1);
+    scene.add(chestLight2);
+    scene.add(chestLight3);
+    scene.add(chestLight4);
+    scene.add(chestLight5);
+}
+
+
+// GO TO DESIRED LEVEL
+function goToLevel(level) {
+    if (level >= 0 && level < levels.length) {
+        currentLevel = level;
+        setupLevel(currentLevel);
+        cube.position.set(0, 0, 0); // Reset the cube position for the new level
+        camera.position.set(40, 40, 40); // Reset the camera position
+    }
+}
+
+
+// 127,15,733
+// 336,15,230
+// 435,15,-172
+// -672,15,-134
+// -375,15,336
+
+
+// BOUNDING BOX SETUP
+var floorBoundingBox = new THREE.Box3();
+var cubeBoundingBox = new THREE.Box3().setFromObject(cube);
+var chestsBoundingBoxes = [];
+var wallsBoundingBoxes = [];
+
+
+// UPDATE BOUNDING BOXES IN RENDER LOOP
+function updateBoundingBoxes() {
+    // Update player's bounding box
+    cubeBoundingBox.setFromObject(cube);
+
+    if (infernoChests && chestsBoundingBoxes.length < 1000) {
+        infernoChests.traverse((child) => {
+            if (child.isMesh) {
+                const chestBoundingBox = new THREE.Box3().setFromObject(child);
+                chestsBoundingBoxes.push(chestBoundingBox);
+            }
+        });
+    }
+
+    if (infernoWalls) {
+        infernoWalls.traverse((child) => {
+            if (child.isMesh) {
+                const wallBoundingBox = new THREE.Box3().setFromObject(child);
+                wallsBoundingBoxes.push(wallBoundingBox);
+            }
+        });
+    }
+}
+
+
+// CHECK FOR COLLISIONS WITH CHESTS
+function checkChestCollisions() {  
+    for (let i = 0; i < chestsBoundingBoxes.length; i++) {
+        if (cubeBoundingBox.intersectsBox(chestsBoundingBoxes[i])) {
+            var x = cube.position.x;
+            var y = cube.position.y;
+            var z = cube.position.z;
+            if(x<=30 && x>=-30 && z<=30 && z>=-30){
+                return false;
+            }
+            flashTimeout = 5000;
+            bounceTimeout = 100;
+            return true; // Collision detected
+        }
+    }
+    return false; // No collision
+}
+
+
+// CHECK FOR COLLISIONS WITH (INVISIBLE) WALLS
+function checkInvisibleWallsCollisions() {
+    for(let i = 0; i< wallsBoundingBoxes.length; i++){
+        if (cubeBoundingBox.intersectsBox(wallsBoundingBoxes[i])) {
+            var x = cube.position.x;
+            var y = cube.position.y;
+            var z = cube.position.z;
+            if(x<=30 && x>=-30 && z<=30 && z>=-30){
+                return false;
+            }
+            return true; // Collision detected
+        }
+    }
+    return false; // No collision
+}
+
+
+// HANDLE ANY COLLISIONS
+function handleCollisions(direction) {
+    // Store the current position
+    const oldCubePosition = cube.position.clone();
+    const oldCameraPosition = camera.position.clone();
+
+    // Try moving the player in the specified direction
+    if (direction === 'forward') {
+        cube.position.z -= moveSpeed;
+        camera.position.z -= moveSpeed;
+    } else if (direction === 'backward') {
+        cube.position.z += moveSpeed;
+        camera.position.z += moveSpeed;
+    } else if (direction === 'left') {
+        cube.position.x -= moveSpeed;
+        camera.position.x -= moveSpeed;
+    } else if (direction === 'right') {
+        cube.position.x += moveSpeed;
+        camera.position.x += moveSpeed;
+    }
+
+    // Update the bounding box after the attempted movement
+    cubeBoundingBox.setFromObject(cube);
+
+    // Check if the player has collided with the floor or a chest
+    // if (!floorBoundingBox.intersectsBox(cubeBoundingBox) || checkChestCollisions()) {
+    if (checkChestCollisions() || checkInvisibleWallsCollisions()) {
+        // If collided, revert to the previous position
+        console.log("I ams stuck");
+        cube.position.copy(oldCubePosition);
+        camera.position.copy(oldCameraPosition);
+    }
+}
+
+
+// UPDATE THE PLAYER'S POSITION
+function updatePlayerPosition() {
+    if (moveForward) {
+        handleCollisions('forward');
+    }
+    if (moveBackward) {
+        handleCollisions('backward');
+    }
+    if (moveLeft) {
+        handleCollisions('left');
+    }
+    if (moveRight) {
+        handleCollisions('right');
+    }
+}
+
+
+// MOVEMENT/CONTROL VARIABLES
+var mouse = new THREE.Vector2();
+var raycaster = new THREE.Raycaster();
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var moveSpeed = 0.75;
+var flashLightDistance = 10;
+
+
+// EVENT LISTENER FOR MOUSE MOVEMENT
+window.addEventListener('mousemove', function(event) {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    if (infernoMap) {  // Check if infernoMap is defined
+        let intersects = raycaster.intersectObject(infernoMap);
+
+        if (intersects.length > 0) {
+            const point = intersects[0].point;
+            flashLightTarget.position.copy(point);
+
+            var dx = point.x - cube.position.x;
+            var dz = point.z - cube.position.z;
+
+            var angleRadians = Math.atan2(dz, dx);
+
+            flashHolder.position.x = flashLightDistance * Math.cos(angleRadians);
+            flashHolder.position.z = flashLightDistance * Math.sin(angleRadians);
+            flashHolder.position.y = 2;
+        }
+    }
+});
+
+
+// EVENT LISTENER FOR KEY PRESSES
+window.addEventListener('keydown', function(event) {
+    switch(event.key) {
+        case 'w': moveForward = true; break;
+        case 's': moveBackward = true; break;
+        case 'a': moveLeft = true; break;
+        case 'd': moveRight = true; break;
+        case 'p': console.log(cube.position); break;
+        case 'l': flashTimeout = 5000; bounceTimeout = 100; break;
+        case 'r': resetLevel(); break;
+        case 'x': flashTimeout = 99; darknessTimeout=10; break;
+    }
+});
+
+
+// STOP MOVING WHEN KEY STOPS BEING PRESSED
+window.addEventListener('keyup', function(event) {
+    switch(event.key) {
+        case 'w': moveForward = false; break;
+        case 's': moveBackward = false; break;
+        case 'a': moveLeft = false; break;
+        case 'd': moveRight = false; break;
+    }
+});
+
+
+// Update resolution on window resize
+window.addEventListener('resize', function() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+
+export function render() {
+
+    // UPDATE PLAYER'S POSITION
+    updatePlayerPosition();
+
+    // UPDATE BOUNDING BOXES
+    updateBoundingBoxes();
+
+    // FLASHLIGHT BATTERY TIMEOUT
+    if (flashTimeout > 100) {
+        darknessTimeout = 100;
+        // FLASHLIGHT FLICKER RANDOMISER
+        if (flickerTimeout === 0 && Math.random() < 0.006){
+            flickerTimeout = 30;
+        }
+        flashTimeout -= 0.9;
+        bounceTimeout -= 0.00018;
+    }else{
+        darknessTimeout -= 0.1;
+    }
+
+    // FLASHLIGHT FLICKERING
+    if (flickerTimeout > 0){
+        flickerTimeout -= 1;
+        switch (flickerTimeout){
+            case 25:
+                flashLight.intensity = 5000;
+                flashLightBounce.intensity = 100;
+            break;
+            case 15:
+                flashLight.intensity = 1;
+                flashLightBounce.intensity = 1;
+            break;
+            case 5:
+                flashLight.intensity = 5000;
+                flashLightBounce.intensity = 100;
+            break;
+            case 1:
+                flashLight.intensity = 1;
+                flashLightBounce.intensity = 1;
+            break;
+        }
+    }
+    else {
+        flashLight.intensity = flashTimeout;
+        flashLightBounce.intensity = bounceTimeout;
+    }
+
+    // SHOW DEATH SCREEN AFTER PLAYER IN DARK FOR TOO LONG
+    if (darknessTimeout <= 0) {
+        showGameOverScreen();
+        flashTimeout = 0;
+        bounceTimeout = 0;
+        resetLevelTimeout -= 0.03;
+    }
+
+    // TIMER TO RESET LEVEL AFTER DEATH
+    if(resetLevelTimeout <= 0){
+        resetLevelTimeout = 10;
+        resetLevel();
+    }
+
+    // VIGNETTE TO WARN PLAYER OF BEING IN DARKNESS
+    const vignetteIntensity = THREE.MathUtils.clamp(1 - (darknessTimeout / 100), 0, 1);
+    updateVignetteIntensity(vignetteIntensity);
+
+    // UPDATE THE ANIMATION FRAME
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+
+// Initial level setup
+// setupLevel(currentLevel);
+// render();
