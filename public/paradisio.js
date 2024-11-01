b6ac3f3fd60d9e28358716b8cf12662b8a1fc0fd
NotJordanZA <jordan.bareiss1@gmail.com>
2024-11-01 18:40:14 +0200

    Added background music to the levels

--- public/paradisio.js
+++ public/paradisio.js
@@ -276,8 +276,17 @@ gltfLoader.load('./assets/level3/cgv-heaven-walls.glb', (gltf) => {
 
 const listener = new THREE.AudioListener();
 camera.add( listener );
-const deathPopupSound = new THREE.Audio( listener );
 const audioLoader = new THREE.AudioLoader();
+
+const backgroundMusic = new THREE.Audio( listener );
+audioLoader.load( './assets/music/paradisio.mp3', function( buffer ) {
+	backgroundMusic.setBuffer( buffer );
+	backgroundMusic.setVolume( 0.5 );
+    backgroundMusic.setLoop(true);
+    backgroundMusic.play();
+});
+
+const deathPopupSound = new THREE.Audio( listener );
 audioLoader.load( './assets/soundeffects/you-died-sting.mp3', function( buffer ) {
 	deathPopupSound.setBuffer( buffer );
 	deathPopupSound.setVolume( 0.5 );
